6d5fb2dee2165755430da9f7780770d151f46253
Satyam Shivhare <mkssshivhare@gmail.com>
2025-05-29 06:37:04 +0530

    Added the wallet UI in the application

---
+++ bholuma-components/Wallet.tsx
@@ -0,0 +1,70 @@
+import * as React from "react"
+import { RefreshCcw } from "lucide-react"
+import { Send, RadioReceiverIcon, Replace } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+
+function Wallet() {
+    return (
+        <Card className="w-[350px] text-left">
+            <CardHeader>
+                <CardTitle>
+                    Overview
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <div>
+                    <div className="text-2xl font-bold">
+                        0.00 SOL
+                    </div>
+                    <div className="text-sm text-muted-foreground">
+                        Available balance
+                    </div>
+                </div>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+                <div>
+                    <Button variant="ghost" className="w-full rounded-lg bg-black">
+                        <RefreshCcw className="h-4 w-4 text-white" />
+                    </Button>
+                    <div className="text-sm text-muted-foreground">
+                        Refresh
+                    </div>
+                </div>
+                <div className="text-center">
+                    <Button variant="outline" className="w-full bg-black rounded-lg">
+                        <Send className="h-4 w-4 text-white" />
+                    </Button>
+                    <div className="text-sm text-muted-foreground">
+                        SOL
+                    </div>
+                </div>
+                <div>
+                    <Button variant="ghost" className="w-full rounded-lg bg-black">
+                        <RadioReceiverIcon className="h-4 w-4 text-white" />
+                    </Button>
+                    <div className="text-sm text-muted-foreground">
+                        Recieve
+                    </div>
+                </div>
+                <div>
+                    <Button variant="ghost" className="w-full rounded-lg bg-black">
+                        <Replace className="h-4 w-4 text-white" />
+                    </Button>
+                    <div className="text-sm text-muted-foreground">
+                        Swap
+                    </div>
+                </div>
+            </CardFooter>
+        </Card>
+    )
+}
+
+export default Wallet
