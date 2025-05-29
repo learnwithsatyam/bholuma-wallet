7ebaea2b56142a657564bd8fdd8810e360cd266e
Satyam Shivhare <mkssshivhare@gmail.com>
2025-05-29 06:37:34 +0530

    added the components in the page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
+"use client";
 import Navbar from "@/bholuma-components/Navbar";
+import Wallet from "@/bholuma-components/Wallet";
 import WalletLogin from "@/bholuma-components/WalletLogin";
+import { RootState } from "@/store/index";
+import {useSelector, useDispatch} from "react-redux";
 
 export default function Home() {
+  const address = useSelector((state: RootState) => state.wallet.address);
+  const privateKey = useSelector((state: RootState) => state.wallet.privateKey);
   return (
     <div>
       <Navbar />
       <div className="flex items-center justify-center h-screen">
         <div className="text-center">
-          <WalletLogin />
+          { address ?
+            <Wallet /> : <WalletLogin />}
         </div>
       </div>
     </div>
