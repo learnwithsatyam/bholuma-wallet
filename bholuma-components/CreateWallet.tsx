3ada4be61d213f6ea4add8bbabb4b4d775c599d8
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-01 08:08:38 +0530

    added CreateWallet.tsx so user can create new wallets

---
+++ bholuma-components/CreateWallet.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from '@/store/index'
+import { setPrivateKey, setAddress } from '@/store/walletSlice'
+import { Card, CardDescription, CardHeader, CardTitle, CardContent, CardFooter, CardAction } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { createWallet } from '@/lib/solanaChain';
+import { Copy } from 'lucide-react';
+
+function CreateWallet({ alreadyHaveWallet, setAlreadyHaveWallet }: { alreadyHaveWallet: boolean, setAlreadyHaveWallet: (value: boolean) => void }) {
+    const dispatch = useDispatch();
+    const [mnumonicPhrase, setMnumonicPhrase] = useState<string | undefined>();
+
+    const handleCreateWallet = () => {
+        var keyPair = createWallet();
+        dispatch(setPrivateKey(keyPair.privateKey));
+        dispatch(setAddress(keyPair.address));
+        setMnumonicPhrase(keyPair.mnemonicPhrase) // Update the mnemonic phrase state
+    }
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Welcome!!</CardTitle>
+                <CardDescription>Create wallet or login to existing wallet...</CardDescription>
+                {mnumonicPhrase && <CardAction onClick={() => navigator.clipboard.writeText(mnumonicPhrase)}><Copy /></CardAction>}
+            </CardHeader>
+            <CardContent>
+                {mnumonicPhrase ?
+                    <div className="text-sm">
+                        <p>Your mnemonic phrase is:</p>
+                        <p className="font-mono">{mnumonicPhrase}</p>
+                        <p>Please keep it safe and secure.</p>
+                        <Button variant="outline" className="w-full rounded-lg mt-4" onClick={() => { setAlreadyHaveWallet(true) }}>
+                            Continue to Wallet
+                        </Button>
+                    </div>
+                    :
+                    <Button variant="secondary" className="w-full rounded-lg" onClick={ handleCreateWallet }>
+                        Create New Wallet
+                    </Button>
+                }
+            </CardContent>
+            <CardFooter className='flex flex-col items-center'>
+                <p className='text-sm text-center'>Already have a wallet?
+                <Button variant="link" className="w-full rounded-lg" onClick={() => { setAlreadyHaveWallet(true) }}>
+                    Sign in
+                </Button>
+                 </p>
+            </CardFooter>
+        </Card>
+    )
+}
+
+export default CreateWallet
